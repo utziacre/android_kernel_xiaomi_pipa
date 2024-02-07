4b9b43213dfcd502581b0356388c41051839a0d3
Chao Yu <chao@kernel.org>
2024-02-07 15:05:48 +0800

    f2fs: fix to create selinux label during whiteout initialization
    generic/700       - output mismatch (see /media/fstests/results//generic/700.out.bad)
        --- tests/generic/700.out	2023-03-28 10:40:42.735529223 +0000
        +++ /media/fstests/results//generic/700.out.bad	2024-02-06 04:37:56.000000000 +0000
        @@ -1,2 +1,4 @@
         QA output created by 700
        +/mnt/scratch_f2fs/f1: security.selinux: No such attribute
        +/mnt/scratch_f2fs/f2: security.selinux: No such attribute
         Silence is golden
        ...
        (Run 'diff -u /media/fstests/tests/generic/700.out /media/fstests/results//generic/700.out.bad'  to see the entire diff)
    
    HINT: You _MAY_ be missing kernel fix:
          70b589a37e1a xfs: add selinux labels to whiteout inodes
    
    Previously, it missed to create selinux labels during whiteout inode
    initialization, fix this issue.
    
    Fixes: 7e01e7ad746b ("f2fs: support RENAME_WHITEOUT")
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -847,7 +847,7 @@ out:
 
 static int __f2fs_tmpfile(struct inode *dir,
 			  struct dentry *dentry, umode_t mode, bool is_whiteout,
-			  struct inode **new_inode)
+			  struct inode **new_inode, struct f2fs_filename *fname)
 {
 	struct f2fs_sb_info *sbi = F2FS_I_SB(dir);
 	struct inode *inode;
@@ -875,7 +875,7 @@ static int __f2fs_tmpfile(struct inode *dir,
 	if (err)
 		goto out;
 
-	err = f2fs_do_tmpfile(inode, dir);
+	err = f2fs_do_tmpfile(inode, dir, fname);
 	if (err)
 		goto release_out;
 
@@ -924,19 +924,21 @@ static int f2fs_tmpfile(struct inode *dir, struct dentry *dentry, umode_t mode)
 	if (!f2fs_is_checkpoint_ready(sbi))
 		return -ENOSPC;
 
-	return __f2fs_tmpfile(dir, dentry, mode, false, NULL);
+	return __f2fs_tmpfile(dir, dentry, mode, false, NULL, NULL);
 }
 
-static int f2fs_create_whiteout(struct inode *dir, struct inode **whiteout)
+static int f2fs_create_whiteout(struct inode *dir, struct inode **whiteout,
+				struct f2fs_filename *fname)
 {
-	return __f2fs_tmpfile(dir, NULL,
-				S_IFCHR | WHITEOUT_MODE, true, whiteout);
+	return __f2fs_tmpfile(dir, NULL, S_IFCHR | WHITEOUT_MODE,
+					true, whiteout, fname);
 }
 
 int f2fs_get_tmpfile(struct inode *dir,
 		     struct inode **new_inode)
 {
-	return __f2fs_tmpfile(dir, NULL, S_IFREG, false, new_inode);
+	return __f2fs_tmpfile(dir, NULL, S_IFREG,
+				false, new_inode, NULL);
 }
 
 static int f2fs_rename(struct inode *old_dir, struct dentry *old_dentry,
@@ -980,7 +982,14 @@ static int f2fs_rename(struct inode *old_dir, struct dentry *old_dentry,
 	}
 
 	if (flags & RENAME_WHITEOUT) {
-		err = f2fs_create_whiteout(old_dir, &whiteout);
+		struct f2fs_filename fname;
+
+		err = f2fs_setup_filename(old_dir, &old_dentry->d_name,
+							0, &fname);
+		if (err)
+			return err;
+
+		err = f2fs_create_whiteout(old_dir, &whiteout, &fname);
 		if (err)
 			return err;
 	}
